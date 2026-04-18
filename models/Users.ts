e8e1a9edbe53d3e356522823cf2469efdadc42ac
vikassax1610 <vikassaxena816@gmail.com>
2026-04-18 16:07:09 +0530

    Add initial database and user models, integrate next-auth for authentication

---
+++ models/Users.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, model, models } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface iUser {
+  email: string;
+  password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema<iUser>(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = models?.User || model<iUser>("User", userSchema);
+
+export default User;
