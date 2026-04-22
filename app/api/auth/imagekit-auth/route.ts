05306a4e56042f718ff3af0ba6e5bbe1483c0cf1
vikassax1610 <vikassaxena816@gmail.com>
2026-04-22 22:57:59 +0530

    Implement authentication with NextAuth and user registration API

---
+++ app/api/auth/imagekit-auth/route.ts
@@ -0,0 +1,13 @@
+import { getUploadAuthParams } from "@imagekit/next/server";
+
+export async function GET() {
+  const authenticationParameters = getUploadAuthParams({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+    publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+  });
+
+  return Response.json({
+    authenticationParameters,
+    publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+  });
+}
