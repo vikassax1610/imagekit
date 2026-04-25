1674273668fcfc017b336fcba08f05ef61db9837
vikassax1610 <vikassaxena816@gmail.com>
2026-04-25 14:58:56 +0530

    Refactor authentication parameter generation to include error handling and add VSCode settings for Postman

--- app/api/auth/imagekit-auth/route.ts
+++ app/api/auth/imagekit-auth/route.ts
@@ -1,13 +1,24 @@
 import { getUploadAuthParams } from "@imagekit/next/server";
 
 export async function GET() {
-  const authenticationParameters = getUploadAuthParams({
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
-    publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
-  });
+  try {
+    const authenticationParameters = getUploadAuthParams({
+      privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY as string,
+    });
 
-  return Response.json({
-    authenticationParameters,
-    publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
-  });
+    return Response.json({
+      authenticationParameters,
+      publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+    });
+  } catch (error) {
+    console.error(
+      "Error occurred while generating authentication parameters in image kit:",
+      error,
+    );
+    return Response.json(
+      { error: "Failed to generate authentication parameters" },
+      { status: 500 },
+    );
+  }
 }
