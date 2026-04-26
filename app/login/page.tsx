b064a135bea75c67347f101a768989f6b97f8576
vikassax1610 <vikassaxena816@gmail.com>
2026-04-26 19:06:23 +0530

    Add login page with form handling and authentication logic

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,56 +1,39 @@
 "use client";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
-export default function Register() {
+import { signIn } from "next-auth/react";
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const router = useRouter();
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault();
     setLoading(true);
-    if (password !== confirmPassword) {
-      setLoading(false);
-
-      alert("Password does not match");
-      return;
-    }
-    try {
-      const response = await axios.post(
-        "/api/auth/register",
-        {
-          email,
-          password,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        },
-      );
-      setLoading(false);
-
-      if (!response.data.success) {
-        throw new Error("failed to register");
-      }
-      router.push("/login");
-    } catch (error) {
-      console.error("Error during registration:", error);
-      alert("An error occurred while registering");
+    const response = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    setLoading(false);
+    if (response?.error) {
+      alert("Email and Password is invalid");
+    } else {
+      router.push("/");
     }
   };
   return (
     <>
+      {" "}
       <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-black to-black">
         <form
           onSubmit={handleSubmit}
           className="backdrop-blur-lg bg-white/10 border border-white/20 shadow-2xl flex flex-col gap-4 max-w-2xl w-full rounded-2xl px-8 py-6"
         >
           <h2 className="text-center text-white text-2xl font-bold">
-            Register Now
+            Login Now
           </h2>
 
           <input
@@ -68,29 +51,18 @@ export default function Register() {
             onChange={(e) => setPassword(e.target.value)}
             className="bg-white/10 text-white placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-lg w-full px-4 py-2 backdrop-blur-md border border-white/20"
           />
-          <input
-            type="password"
-            placeholder="Confirm password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            className="bg-white/10 text-white placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-lg w-full px-4 py-2 backdrop-blur-md border border-white/20"
-          />
 
           <button
             type="submit"
             disabled={loading}
             className="bg-[#D1143E] hover:bg-red-600 transition-all text-white font-semibold py-2 rounded-lg shadow-lg"
           >
-            {loading ? "registering..." : "register"}
+            {loading ? "Logging in..." : "Log in"}
           </button>
           <p className="text-white text-center mt-4">
-            Already have an account?{" "}
-            <Link
-              href="/login"
-              onClick={() => router.push("/login")}
-              className="text-blue-500 hover:underline"
-            >
-              Login
+            Don`t have account{" "}
+            <Link href="/register" className="text-blue-500 hover:underline">
+              create one
             </Link>
           </p>
         </form>
